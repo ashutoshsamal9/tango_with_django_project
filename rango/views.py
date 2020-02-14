2e9e608daf9990ba7b30c5d7ff6e4018b2b10b9b
“Ashutosh <2407720s@student.gla.ac.uk>
2020-02-14 18:26:36 +0000

    Added Cookies

--- rango/views.py
+++ rango/views.py
@@ -19,6 +19,7 @@ def index(request):
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
+	visitor_cookie_handler(request)
 	# Render the response and send it back!
 	return render(request, 'rango/index.html', context=context_dict)
 
@@ -32,7 +33,8 @@ def show_category(request, category_name_slug):
 	# Create a context dictionary which we can pass
 	# to the template rendering engine.
 	context_dict = {}
-	
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 	try:
 		# Can we find a category name slug with the given name?
 		# If we can't, the .get() method raises a DoesNotExist exception.
@@ -169,3 +171,22 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    request.session['visits'] = visits
